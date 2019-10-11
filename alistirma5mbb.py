0514d9a2e5c1e684508d9d40d58ca981dd291057
mustafaberkb <mustafaberkb@users.noreply.github.com>
2019-10-11 20:27:50 +0300

    Update alistirma5mbb.py

--- alistirma5mbb.py
+++ alistirma5mbb.py
@@ -1,7 +1,4 @@
+#4 basamaklı palindromik sayıları yazdıran program
 for i in range(1,10):
 	for j in range (0,10):
-		for k in range (0,10):
-			for l in range(0,10):
-				if i==l:
-					if j==k:
-						print(1000*i+100*j+10*k+l)
+		print(1000*i+100*j+10*j+i)
