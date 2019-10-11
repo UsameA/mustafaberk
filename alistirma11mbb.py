02c482a84ee374b4e6305f1dc469122fc2eb35e4
mustafaberkb <mustafaberkb@users.noreply.github.com>
2019-10-11 20:27:38 +0300

    Update alistirma11mbb.py

--- alistirma11mbb.py
+++ alistirma11mbb.py
@@ -1,4 +1,6 @@
+#350 200 ve 150 sayılarını böldüğünde aynı kalanı veren en büyük sayıyı veren program
 for i in range(1,350):
 	if 350%i==200%i==125%i:
-		print(i)
-#hepsini yazıyorum fakat en büyüğü nasıl çekeceğimi anlayamadım
+		c=i
+print(c)
+
