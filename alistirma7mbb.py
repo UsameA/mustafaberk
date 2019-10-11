636034e116db5f748522d8e5b185621dc4cf3627
mustafaberkb <mustafaberkb@users.noreply.github.com>
2019-10-11 20:27:52 +0300

    Update alistirma7mbb.py

--- alistirma7mbb.py
+++ alistirma7mbb.py
@@ -1,5 +1,9 @@
+#ilk iki rakamının toplamı son rakamına eşit olan sayılar ve kaç tane olduğunu veren program
+c=0
 for i in range(1,10):
 	for j in range (0,10):
 		for k in range (0,10):
 			if i+j==k:
 				print(100*i+10*j+k)
+				c=c+1
+print("sayı adedi",c)
